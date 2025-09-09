c594fe3e6e0b598b37accfd1daadc6d312742b83
boburmirzo-brm <boburmirzo.brm@gmail.com>
2025-09-09 11:26:50 +0500

    dynamic route

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,7 @@
-import { memo } from "react";
+import { memo, use } from "react";
 
-const Page = async ({ params }: { params: { id: string } }) => {
-  const {id} = await params;
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
   const response = await fetch(`https://dummyjson.com/products/${id}`);
   const data = await response.json();
 
